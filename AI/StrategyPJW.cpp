458950982ff6a7fce4ff1cafcf43dd1ad60cf905
pengjiawen <pengjiawen@shengqugames.com>
2020-09-03 17:18:33 +0800

    StrategyPJW 录像数据训练

--- AI/StrategyPJW.cpp
+++ AI/StrategyPJW.cpp
@@ -5,7 +5,7 @@
 using namespace MiniDNN;
 
 #define XDIM 4 // (enemy pos, enemy next pos)*2; (bullet pos, bullet next pos)*2
-#define HDIM 20
+#define HDIM 32
 #define YDIM 6
 
 StrategyPJW::StrategyPJW() : CStrategy(XDIM, YDIM) { valid = true; }
@@ -16,56 +16,14 @@ OutputData StrategyPJW::MakeDecision(const InputData & id)
 	OutputData od;
 	if (id.enemies_pos.size() > 0)
 	{
-		// collect data
-		int px = id.player_pos.x;
-		int py = id.player_pos.y;
-		int ex = id.enemies_pos[0].x;
-		int ey = id.enemies_pos[0].y;
-		int dx = 0;
-		int dy = 0;
-		DIRECTION ed = id.enemies_dir[0];
-		if (ed == DIR_UP && ey < 24) dy = 1;
-		else if (ed == DIR_DOWN && ey > 3) dy = -1;
-		else if (ed == DIR_LEFT && ex > 1) dx = -1;
-		else if (ed == DIR_RIGHT && ex < 24) dx = 1;
-		// tank collision
-		if (ex >= -2 && ex < 0 && dx > 0) dx = 0;
-		if (ex <= 2 && ex > 0 && dx < 0) dx = 0;
-		if (ey >= -2 && ey < 0 && dy > 0) dy = 0;
-		if (ey <= 2 && ey > 0 && dy < 0) dy = 0;
-
-		// normalize data
-		ex = ex - px;
-		ey = ey - py;
+		ConvertData(id);
 		DIRECTION pd = id.player_dir;
-		if (pd == DIR_DOWN)
-		{
-			ex = -ex; ey = -ey;
-			dx = -dx; dy = -dy;
-		}
-		else if (pd == DIR_LEFT)
-		{
-			int tmp = ex; ex = ey; ey = -tmp;
-			tmp = dx; dx = dy; dy = -tmp;
-		}
-		else if (pd == DIR_RIGHT)
-		{
-			int tmp = ex; ex = -ey; ey = tmp;
-			tmp = dx; dx = -dy; dy = tmp;
-		}
-
 		// make decision
 		// Hard Code
-		//od = MakeDecisionHardCode(ex, ey, dx, dy);
+		//od = MakeDecisionHardCode(input(0,0), input(1, 0), input(2, 0), input(3, 0));
 		//ConvertData(od);
 
 		// NN
-		//input(0, 0) = (float)ex / 26.0f;
-		//input(1, 0) = (float)ey / 26.0f;
-		input(0, 0) = (float)ex;
-		input(1, 0) = (float)ey;
-		input(2, 0) = dx;
-		input(3, 0) = dy;
 		output = network.predict(input);
 		// 找到最大概率的移动方向
 		int label = 4; // 默认为None
@@ -116,31 +74,6 @@ OutputData StrategyPJW::MakeDecision(const InputData & id)
 		od.shoot = false;
 	}
 	return od;
-
-	//OutputData od;
-	//if (!valid)
-	//{
-	//	std::cout << "[Strategy] MakeDecision Failed. Network is not valid.\n";
-	//	return od;
-	//}
-	//ConvertData(id);
-	//output = network.predict(input);
-
-	//// 找到最大概率的移动方向
-	//int label = 4; // 默认为None
-	//float max = -1.0f;
-	//for (int i = 0; i < 5; i++)
-	//{
-	//	float value = output(i, 0);
-	//	if (value > max)
-	//	{
-	//		max = value;
-	//		label = i;
-	//	}
-	//}
-	//od.mov = (DIRECTION)label;
-	//od.shoot = output(5, 0) > 0.5 ? true : false;
-	//return od;
 }
 
 void StrategyPJW::Train(const IODataVec & database, std::string folder, std::string fileName, float learning_rate, int batch_size, int epoch)
@@ -148,40 +81,57 @@ void StrategyPJW::Train(const IODataVec & database, std::string folder, std::str
 	SetupNetwork();
 
 	int dataCount = database.size();
-	/*int uselessCnt(0);
+
+	Matrix x(x_dim, dataCount);
+	Matrix y(y_dim, dataCount);
+
+	int writeIdx(0);
 	for (int i = 0; i < dataCount; i++)
 	{
 		const InputData& id = database[i].first;
-		if (id.player_bullet_pos.x != -1) {
-			uselessCnt++; continue;
-		}
 		const OutputData& od = database[i].second;
-		if (!od.shoot && od.mov == DIR_NONE) {
-			uselessCnt++; continue;
+
+		ConvertData(id);
+		DIRECTION pd = id.player_dir;
+		OutputData nod = od;
+		if (nod.mov == DIR_UP)
+		{
+			if (pd == DIR_UP) nod.mov = DIR_UP;
+			else if (pd == DIR_DOWN) nod.mov = DIR_DOWN;
+			else if (pd == DIR_LEFT) nod.mov = DIR_RIGHT;
+			else if (pd == DIR_RIGHT) nod.mov = DIR_LEFT;
 		}
+		else if (od.mov == DIR_DOWN)
+		{
+			if (pd == DIR_UP) nod.mov = DIR_DOWN;
+			else if (pd == DIR_DOWN) nod.mov = DIR_UP;
+			else if (pd == DIR_LEFT) nod.mov = DIR_LEFT;
+			else if (pd == DIR_RIGHT) nod.mov = DIR_RIGHT;
+		}
+		else if (nod.mov == DIR_LEFT)
+		{
+			if (pd == DIR_UP) nod.mov = DIR_LEFT;
+			else if (pd == DIR_DOWN) nod.mov = DIR_RIGHT;
+			else if (pd == DIR_LEFT) nod.mov = DIR_UP;
+			else if (pd == DIR_RIGHT) nod.mov = DIR_DOWN;
+		}
+		else if (nod.mov == DIR_RIGHT)
+		{
+			if (pd == DIR_UP) nod.mov = DIR_RIGHT;
+			else if (pd == DIR_DOWN) nod.mov = DIR_LEFT;
+			else if (pd == DIR_LEFT) nod.mov = DIR_DOWN;
+			else if (pd == DIR_RIGHT) nod.mov = DIR_UP;
+		}
+		ConvertData(nod);
+
+		for (int k = 0; k < x_dim; k++) x(k, writeIdx) = input(k, 0);
+		for (int k = 0; k < y_dim; k++) y(k, writeIdx) = output(k, 0);
+		writeIdx++;
 	}
-	std::cout << "Useless Count: " << uselessCnt << std::endl;*/
 
-	//Matrix x(x_dim, dataCount);
-	//Matrix y(y_dim, dataCount);
-
-	//int writeIdx(0);
-	//for (int i = 0; i < dataCount; i++)
-	//{
-	//	const InputData& id = database[i].first;
-	//	//if (id.player_bullet_pos.x != -1) continue;
-	//	const OutputData& od = database[i].second;
-	//	//if (!od.shoot && od.mov == DIR_NONE) continue;
-
-	//	ConvertData(id);
-	//	ConvertData(od);
-
-	//	for (int k = 0; k < x_dim; k++) x(k, writeIdx) = input(k, 0);
-	//	for (int k = 0; k < y_dim; k++) y(k, writeIdx) = output(k, 0);
-	//	writeIdx++;
-	//}
-	Matrix x, y;
-	MakeFakeData(x, y);
+	// Fake Data
+	//Matrix x, y;
+	//MakeFakeData(x, y);
 
 	// WangLiang: Dump data to csv --------------------------------------------
 	{
@@ -218,7 +168,7 @@ void StrategyPJW::Train(const IODataVec & database, std::string folder, std::str
 
 	network.init(0, 0.01, 123);
 
-	network.fit(opt, x, y, 512, 200);
+	network.fit(opt, x, y, 32, 200);
 
 	valid = true;
 
@@ -280,99 +230,69 @@ void StrategyPJW::SetupNetwork()
 	//network.add_layer(layer111);
 	network.add_layer(layer2);
 
-	//network.set_output(new RegressionMSE());
-	network.set_output(new MultiClassEntropy());
+	network.set_output(new RegressionMSE());
+	//network.set_output(new MultiClassEntropy());
 }
 
 void StrategyPJW::ConvertData(const InputData & id)
 {
-	const float inverse26 = 1.0f / 26.0f;
-	int writeIdx(0);
-	float px = id.player_pos.x + 0.5f;
-	float py = id.player_pos.y + 0.5f;
-
-	if (id.player_bullet_pos.x != -1) input(writeIdx++, 0) = 1;
-	else input(writeIdx++, 0) = 0;
-
-	//input(writeIdx++, 0) = px * inverse26;
-	//input(writeIdx++, 0) = py * inverse26;
-	/*input(writeIdx++, 0) = 0;
-	input(writeIdx++, 0) = 0;
-	input(writeIdx++, 0) = 0;
-	input(writeIdx++, 0) = 0;
-	input(id.player_dir, 0) = 1;*/
-
-	// enemy positon, deltaPosition
-	int enemyCnt = id.enemies_pos.size();
-	for (int i = 0; i < 1; i++)
+	if (id.enemies_pos.size() == 0)
 	{
-		if (i < enemyCnt)
-		{
-			float ex = id.enemies_pos[i].x + 0.5f;
-			float ey = id.enemies_pos[i].y + 0.5f;
-			float dx = 0; // enemy delta x
-			float dy = 0; // enemy delta y
-			input(writeIdx++, 0) = (ex - px) / 26.0f;
-			input(writeIdx++, 0) = (ey - py) / 26.0f;
-			switch (id.enemies_dir[i])
-			{
-			case DIR_UP:
-				if (ey + 0.5f < 25.0f) dy = inverse26; break;
-			case DIR_DOWN:
-				if (ey - 0.5f > 0.0f) dy = -inverse26; break;
-			case DIR_LEFT:
-				if (ex - 0.5f > 0.0f) dx = -inverse26; break;
-			case DIR_RIGHT:
-				if (ex + 0.5f < 25.0f) dx = inverse26; break;
-			}
-			input(writeIdx++, 0) = dx;
-			input(writeIdx++, 0) = dy;
-		}
-		else
-		{
-			input(writeIdx++, 0) = 0;
-			input(writeIdx++, 0) = 0;
-			input(writeIdx++, 0) = 0;
-			input(writeIdx++, 0) = 0;
-		}
+		input(0, 0) = 999;
+		input(1, 0) = 999;
+		input(2, 0) = 999;
+		input(3, 0) = 999;
+		return;
+	}
+	int px = id.player_pos.x;
+	int py = id.player_pos.y;
+	int ex = id.enemies_pos[0].x;
+	int ey = id.enemies_pos[0].y;
+	int dx = 0;
+	int dy = 0;
+	DIRECTION ed = id.enemies_dir[0];
+	if (ed == DIR_UP && ey < 24) dy = 1;
+	else if (ed == DIR_DOWN && ey > 3) dy = -1;
+	else if (ed == DIR_LEFT && ex > 1) dx = -1;
+	else if (ed == DIR_RIGHT && ex < 24) dx = 1;
+	// tank collision
+	if (ex >= -2 && ex < 0 && dx > 0) dx = 0;
+	if (ex <= 2 && ex > 0 && dx < 0) dx = 0;
+	if (ey >= -2 && ey < 0 && dy > 0) dy = 0;
+	if (ey <= 2 && ey > 0 && dy < 0) dy = 0;
+
+	// normalize data
+	ex = ex - px;
+	ey = ey - py;
+	DIRECTION pd = id.player_dir;
+	if (pd == DIR_DOWN)
+	{
+		ex = -ex; ey = -ey;
+		dx = -dx; dy = -dy;
+	}
+	else if (pd == DIR_LEFT)
+	{
+		int tmp = ex; ex = ey; ey = -tmp;
+		tmp = dx; dx = dy; dy = -tmp;
+	}
+	else if (pd == DIR_RIGHT)
+	{
+		int tmp = ex; ex = -ey; ey = tmp;
+		tmp = dx; dx = -dy; dy = tmp;
 	}
 
-	// bullet position, deltaPosition
-	//int bulletCnt = id.enemies_bullet_pos.size();
-	//for (int i = 0; i < 1; i++)
-	//{
-	//	if (i < bulletCnt)
-	//	{
-	//		float bx = id.enemies_bullet_pos[i].x + 0.5f;
-	//		float by = id.enemies_bullet_pos[i].y + 0.5f;
-	//		float dx = 0; // enemy delta x
-	//		float dy = 0; // enemy delta y
-	//		input(writeIdx++, 0) = (bx - px) / 26.0f;
-	//		input(writeIdx++, 0) = (by - py) / 26.0f;
-	//		switch (id.enemies_bullet_dir[i])
-	//		{
-	//		case DIR_UP:
-	//			if (by + 0.5f < 25.0f) dy = inverse26; break;
-	//		case DIR_DOWN:
-	//			if (by - 0.5f > 0.0f) dy = -inverse26; break;
-	//		case DIR_LEFT:
-	//			if (bx - 0.5f > 0.0f) dx = -inverse26; break;
-	//		case DIR_RIGHT:
-	//			if (bx + 0.5f < 25.0f) dx = inverse26; break;
-	//		}
-	//		input(writeIdx++, 0) = dx;
-	//		input(writeIdx++, 0) = dy;
-	//	}
-	//	else
-	//	{
-	//		input(writeIdx++, 0) = 0;
-	//		input(writeIdx++, 0) = 0;
-	//		input(writeIdx++, 0) = 0;
-	//		input(writeIdx++, 0) = 0;
-	//	}
-	//}
+	// make decision
+	// Hard Code
+	//od = MakeDecisionHardCode(ex, ey, dx, dy);
+	//ConvertData(od);
 
-	if (writeIdx != XDIM) std::cerr << "write index error" << std::endl;
+	// NN
+	//input(0, 0) = (float)ex / 26.0f;
+	//input(1, 0) = (float)ey / 26.0f;
+	input(0, 0) = (float)ex;
+	input(1, 0) = (float)ey;
+	input(2, 0) = dx;
+	input(3, 0) = dy;
 }
 
 void StrategyPJW::ConvertData(const OutputData & od)
